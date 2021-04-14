40df8656cb6192b4184a375a93d9ccbe9333f399
hhs66317 <hhs66317@gmail.com>
2021-04-14 17:47:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,29 +10,36 @@ function record {
 }
 
 function block_for_change {
-  inotifywait --exclude "(m3u8?)$" \
+  inotifywait \
     --event modify,move,create,delete \
     $PLAYLISTS_DIR
 }
 
 function build {
-  for path in ${PLAYLISTS_DIR}/*.m3u*; do
-  file=`basename $path`
 
-  m3u8_path="${path}"
-  mp4_path="${RECORDINGS_DIR}/${file%.*}.mkv"
+  for line in `cat ${PLAYLISTS_DIR}/list.txt`
+  do
+    readarray -d '|' -t item_arr <<<"$line"
 
-  if [ -f ${mp4_path} ]; then
-    echo "Skipping ${file}, already recorded!"
-  else
-    echo "${m3u8_path} > ${mp4_path}"
-    record "${m3u8_path}" "${mp4_path}"
-  fi
+    m3u8_path="${item_arr[1]}"
+    mp4_path="${RECORDINGS_DIR}/${item_arr[0]}"
 
-  echo "Everything is done!"
-done
+    echo "mp4_path: ${mp4_path}"
+    echo "m3u8_path: ${m3u8_path}"
+
+    if [ -f ${mp4_path} ]; then
+      echo "Skipping ${item_arr[0]}, already recorded!"
+    else
+      echo "${m3u8_path} > ${mp4_path}"
+      record "${m3u8_path}" "${mp4_path}"
+    fi
+
+    echo "Everything is done!"
+  done
 }
+
 build
+
 while block_for_change; do
   build
 done
